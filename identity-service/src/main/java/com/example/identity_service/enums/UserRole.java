d54e230b85f3ec475dc5e55af2661eb6a5c331ad
GiaHuy1007 <phangiahuy10072006@gmail.com>
2025-12-01 04:02:03 +0700

    CRUD user and handler exception

---
+++ identity-service/src/main/java/com/example/identity_service/enums/UserRole.java
@@ -0,0 +1,20 @@
+package com.example.identity_service.enums;
+
+/**
+ * ============================================================================
+ * Project      : project
+ * File         : UserRole.java
+ * Description  : TODO - Mô tả ngắn gọn chức năng của class này.
+ * <p>
+ * Author       : Gia Huy
+ * Created On   : 12/1/2025 4:01 AM
+ * Last Updated : 12/1/2025
+ * <p>
+ * Package      : com.example.identity_service.enums
+ * Version      : 1.0
+ * <p>
+ * Copyright © 12/1/2025. All rights reserved.
+ * ============================================================================
+ */
+public enum UserRole {
+}
